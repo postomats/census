f1281bbc346e24f979a253ba1aa602c1e86d7e5d
Ilya M <i@ilyamaslov.ru>
2024-04-12 01:06:56 +0300

    add migration

---
+++ api/migrations.py
@@ -0,0 +1,21 @@
+from api.models import User
+from db import SessionLocal
+
+admin = User(
+        first_name='admin',
+        last_name='admin',
+        group='',
+        email='admin@admin.ru',
+        password='admin',
+        role='Admin',
+        )
+
+admin.set_password(password='admin')
+db = SessionLocal()
+
+try:
+    if not db.query().filter(User.email == 'admin@admin.ru').first():
+        db.add(admin)
+        db.commit()
+except:
+    pass
