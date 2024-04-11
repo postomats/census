7579a7c952d67c6c5e5accea8a684b2702d00441
Ilya M <i@ilyamaslov.ru>
2024-04-11 19:20:00 +0300

    Revert "add settings for OPENAPI_URL"
    This reverts commit ce4bc95c53172d8278f93ab1744e07c591152283.

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.controller import controller
-from os import environ
 # Создание экземпляра FastAPI
 app = FastAPI()
-openapi_url = environ.get('OPENAPI_URL', '')
+
 # Подключение роутера из модуля api.controller с префиксом и тегом
-app.include_router(controller, tags=["Переписчик)"], openapi_url=openapi_url)
+app.include_router(controller, tags=["Переписчик)"])
 
 # Добавление middleware для обработки CORS
 app.add_middleware(
